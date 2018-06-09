5510d90180b17555da3eb26a1916403b30d74c05
Nicole Eidi <nicoleeidi1@gmail.com>
2018-06-09 14:23:44 +0000

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,21 @@
+// ./src/components/todos/CreateTodo.js
+
 import React, { Component } from 'react'
 
 class CreateTodo extends Component {
   render() {
     return(
       <div>
-        Create Todo Component
+        <form>
+          <p>
+            <label>add todo</label>
+            <input type="text" />
+          </p>
+          <input type="submit" />
+        </form>
       </div>
-    )
+    );
   }
-}
+};
 
 export default CreateTodo;
